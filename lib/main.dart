728379d15aaffaffc091c9ea7ef3a93aa5ebbab5
Snehil <snehilsneh888@gmail.com>
2021-09-05 11:42:08 +0530

    First commit with initial files

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'expenses_app',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Expenses'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
+      ),
+    );
+  }
+}
